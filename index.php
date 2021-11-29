d0a1ffd65047536dbb0dfee62b556fd827b2b191
= <japhet.pagarigan@gmail.com>
2021-11-29 11:32:13 +0800

    added html

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo 'another test';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test Project</title>
+</head>
+<body>
+    <h2>Hello World - Test PHP Project</h2>
+</body>
+</html>
